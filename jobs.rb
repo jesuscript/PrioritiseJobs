b2e874c9c622d2cba7a77b8c4856531cd4f80c17
Megatv <konstantin.k.ed@gmail.com>
2012-06-17 20:43:15 +0100

    first commit

---
+++ jobs.rb
@@ -0,0 +1,17 @@
+class Prioritise
+  def run(jobs)
+    #STUB
+    return []
+  end
+end
+
+class TestPrioritise
+end
+
+
+if __FILE__ == $0
+  prioritise = Prioritise.new(ARGV)
+  prioritise.run("")
+end
+  
+  
